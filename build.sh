78345204d57f1cd0fd14474280f6f0362070b161
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-23 17:48:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 set -o errexit
 
-pip install --upgrade pip
-pip install -r requirements.txt
+# Upgrade pip
+pip3 install --upgrade pip
+
+# Install Python dependencies
+pip3 install -r requirements.txt
+
+# Install Playwright without switching to the root user
+PLAYWRIGHT_BROWSERS_PATH=$(npm config get prefix)/lib/node_modules/playwright
+npm install playwright
+export PATH=$PLAYWRIGHT_BROWSERS_PATH:$PATH
+
+# Install Playwright dependencies
 playwright install
 playwright install-deps
