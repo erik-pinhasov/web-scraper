97401d6c2333155b5e900d0feb7d51dcd36bf695
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-23 18:11:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
 set -o errexit
 
-# Extract the password from the environment variable
-PASSWORD=$APP_PASSWORD
-
-# Upgrade pip
-pip3 install --upgrade pip
-
-# Install Python dependencies
-pip3 install -r requirements.txt
-
-# Install Playwright without switching to the root user
-PLAYWRIGHT_BROWSERS_PATH=$(npm config get prefix)/lib/node_modules/playwright
-npm install playwright
-export PATH=$PLAYWRIGHT_BROWSERS_PATH:$PATH
-
-# Install Playwright dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
 playwright install
-playwright install-deps
