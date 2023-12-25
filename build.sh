01f8c40b9ac65e2ae52014fee8d25ebb859e02ca
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 18:37:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ pip install -r /opt/render/project/requirements.txt
 npm install -g playwright
 npx playwright install
 cd /opt/render/project/src
+ls /opt/render/project/src
+ls /opt/render/project/src/src
+ls /opt/render/project/src/web_app
