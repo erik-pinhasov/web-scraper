95d22445c2a9f5360420a1bf7e182159822ee84e
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 18:31:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,9 @@
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $PROJECT_DIR
-cd $PROJECT_DIR
-ls -l
-echo ----------------
-# Set working directory
-cd /opt/render/project/src
-ls -l
-# Install npm
 apt-get update
 apt-get install -y npm
-# Upgrade pip
 pip install --upgrade pip
-
-# Install additional dependencies
-pip install -r $PROJECT_DIR/requirements.txt
-
+cd /opt/render/project/src
+pip install -r /opt/render/project/requirements.txt
 npm install -g playwright
-
 npx playwright install
-
-export PYTHONPATH=$PROJECT_DIR/src
-ls -l
+cd /opt/render/project/src
+export PYTHONPATH=/opt/render/project/src
