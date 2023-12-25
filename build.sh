522fff4b40da5b47363b666bf075a19360765fb6
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 16:10:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@ pip install --upgrade pip
 # Install additional dependencies
 pip install -r $PROJECT_DIR/requirements.txt
 
-su npm install -g playwright
+npm install -g playwright
 
 # Install Playwright dependencies
-su playwright install
-su playwright install-deps
+playwright install
