b74b4c96ec7e1813412f2e56b5ee5f29ffc95758
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 18:27:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $PROJECT_DIR
+cd $PROJECT_DIR
+ls -l
+echo ----------------
 # Set working directory
-
+cd /opt/render/project/src
+ls -l
 # Install npm
 apt-get update
 apt-get install -y npm
