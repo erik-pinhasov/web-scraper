2a54108387aced58ff22654e8334d6b8b7d6d87c
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 19:29:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ apt-get install -y npm
 pip install --upgrade pip
 pip install -r /opt/render/project/src/requirements.txt
 npm install -g playwright
-export PYTHONPATH=/opt/render/project/python
+ls -l /opt/render/project/python
 npx playwright install
