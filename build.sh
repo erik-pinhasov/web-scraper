980252be2325ad0898474f30dec03c6dfe6d6a92
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 19:34:38 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pip install --upgrade pip
 pip install -r /opt/render/project/src/requirements.txt
 npm install -g playwright
 npx playwright install
-export PYTHONPATH=/opt/render/project/python/src/src
+export PYTHONPATH=/opt/render/project/python/src/src/scrapers:/opt/render/project/python/src/src/web_app
