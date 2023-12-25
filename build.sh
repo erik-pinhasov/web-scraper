c9c5715462fcbdaa8d8045a49cf509eafc18f20b
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 16:26:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
-echo ls
-cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+
+# Set Python path
+export PYTHONPATH=./src
+
 # Upgrade pip
 pip install --upgrade pip
 
 # Install additional dependencies
 pip install -r requirements.txt
 
+# Install Playwright
 npm install -g playwright
 
-# Install Playwright dependencies
-playwright install
+# Install Playwright dependencies (specifying the platform explicitly)
+PLAYWRIGHT_BROWSERS_PATH=./path/to/playwright/browsers/ npm install playwright
+PLAYWRIGHT_BROWSERS_PATH=./path/to/playwright/browsers/ playwright install
+PLAYWRIGHT_BROWSERS_PATH=./path/to/playwright/browsers/ playwright install-deps
