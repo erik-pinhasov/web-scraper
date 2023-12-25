63dd6a545410f7d04a85833ff974b321cb0c3d09
erik-pinhasov <96497924+erik-pinhasov@users.noreply.github.com>
2023-12-25 17:15:35 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,22 @@ pip install -r $PROJECT_DIR/requirements.txt
 npm install -g playwright
 
 npx playwright install
-cd /opt/render/project
+PATH_TO_FILE='/opt/render/project/src/web_app/app.py'
+
+# Extract the directory names
+IFS='/' read -ra DIRS <<< "$PATH_TO_FILE"
+
+# Iterate through the directories
+for dir in "${DIRS[@]}"; do
+    # Print the current directory name
+    echo "$dir"
+
+    # Change to the current directory and perform ls
+    ls "$dir"
+
+    # If you want to list files recursively, uncomment the line below
+    # ls -R "$dir"
+
+    # Move into the current directory
+    cd "$dir" || exit 1
+done
